b1c47a0474b4716c14ceb3e81ae87c8aef536a4e
dCOMMICS <sanwurld@gmail.com>
2024-02-21 01:39:09 +0300

    ❤🌱

---
+++ fuck/cool-intro/rt/yahoo-boy/src/App.js
@@ -0,0 +1,15 @@
+import React, {useRef} from 'react';
+import './App.css';
+import { TweenMax,Power3  } from 'gsap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FUCK YOUR FEELINGS UMBWA</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// C:\Users\ADMIN\Desktop\derrick\fuck\cool-intro\rt
