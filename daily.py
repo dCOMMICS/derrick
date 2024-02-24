c5cb809a09f1f361d9674c9f6044e5f49a45e525
dCOMMICS <sanwurld@gmail.com>
2024-02-25 00:06:44 +0300

    q2

--- daily.py
+++ daily.py
@@ -49,3 +49,25 @@ class Solution:
           uf.reset(person)
 
     return [i for i in range(n) if uf.connected(i, 0)]
+
+
+
+
+
+
+#  question 2
+
+class Solution:
+  def buyChoco(self, prices: List[int], money: int) -> int:
+    min1 = math.inf
+    min2 = math.inf
+
+    for price in prices:
+      if price <= min1:
+        min2 = min1
+        min1 = price
+      elif price < min2:
+        min2 = price
+
+    minCost = min1 + min2
+    return money if minCost > money else money - minCost
