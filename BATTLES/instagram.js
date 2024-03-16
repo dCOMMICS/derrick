3524e9b8d72785d22ff1358b231f323b0ab56a7b
dCOMMICS <sanwurld@gmail.com>
2024-03-16 18:45:00 +0300

    working with gsap

--- BATTLES/instagram.js
+++ BATTLES/instagram.js
@@ -1,21 +1,138 @@
-gsap.registerPlugin (ScrollTrigger);
+// gsap.registerPlugin (ScrollTrigger);
 
-const container = 
-document.querySelector(".container");
+// const container = 
+// document.querySelector(".container");
 
-const sections = 
-gsap.utils.toArray (".container .panel"),
+// const sections = 
+// gsap.utils.toArray (".container .panel"),
 
-gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
-    ease: "none",
-    ScrollTrigger: {
-        trigger: ".container",
-        pin: true,
-        scrub: 1,
-        snap : 1 / (sections.length - 1),
-        end:
+// gsap.to(sections, {
+//     xPercent: -100 * (sections.length - 1),
+//     ease: "none",
+//     ScrollTrigger: {
+//         trigger: ".container",
+//         pin: true,
+//         scrub: 1,
+//         snap : 1 / (sections.length - 1),
+//         end:
 
-        ()=> "+=" + container.offsetWidth,
-    },
-});
+//         ()=> "+=" + container.offsetWidth,
+//     },
+// });
+
+gsap.registerPlugin(ScrollTrigger)
+
+const Scroll = new function() {
+	let sections
+	let page
+	let main
+	let scrollTrigger
+	let tl
+	let win
+	
+	// Init
+	this.init = () => {
+		sections = document.querySelectorAll('section')
+		page = document.querySelector('#page')
+		main = document.querySelector('main')
+		win = {
+			w: window.innerWidth,
+			h: window.innerHeight
+		}
+		
+		this.setupTimeline()
+		this.setupScrollTrigger()
+		window.addEventListener('resize', this.onResize)
+	}
+	
+	// Setup ScrollTrigger
+	this.setupScrollTrigger = () => {
+		page.style.height = (this.getTotalScroll() + win.h) + 'px'
+		
+		scrollTrigger = ScrollTrigger.create({
+			id: 'mainScroll',
+			trigger: 'main',
+			animation: tl,
+			pin: true,
+			scrub: true,
+			snap: {
+				snapTo: (value) => {
+					
+					let labels = Object.values(tl.labels)
+					
+					const snapPoints = labels.map(x => x / tl.totalDuration());
+					const proximity = 0.1
+					
+					console.log(tl.labels , tl.totalDuration(), labels, snapPoints)
+					
+					for (let i = 0; i < snapPoints.length; i++) {
+						if (value > snapPoints[i] - proximity && value < snapPoints[i] + proximity) {
+							return snapPoints[i]
+						}
+					}
+				},
+				duration: { min: 0.2, max: 0.6 },
+			},
+			start: 'top top',
+			end: '+=' + this.getTotalScroll(),
+		})
+	}
+	
+	// Setup timeline
+	this.setupTimeline = () => {
+		tl = gsap.timeline()
+		tl.addLabel("label-initial")
+		
+		sections.forEach((section, index) => {
+			const nextSection = sections[index+1]
+			if (!nextSection) return
+
+			tl.to(nextSection, {
+				y: -1 * nextSection.offsetHeight,
+				duration: nextSection.offsetHeight,
+				ease: 'linear',
+			})
+			.addLabel(`label${index}`)
+		})
+	}
+	
+	// On resize
+	this.onResize = () => {
+		win = {
+			w: window.innerWidth,
+			h: window.innerHeight
+		}
+		
+		this.reset()
+	}
+	
+	// Reset
+	this.reset = () => {
+		if (typeof ScrollTrigger.getById('mainScroll') === 'object') {
+			ScrollTrigger.getById('mainScroll').kill()
+		}
+		
+		if (typeof tl === 'object') {
+			tl.kill()
+			tl.seek(0)
+		}
+		
+		document.body.scrollTop = document.documentElement.scrollTop = 0
+		this.init()
+	}
+	
+	// Get total scroll
+	this.getTotalScroll = () => {
+		let totalScroll = 0
+		sections.forEach(section => {
+			totalScroll += section.offsetHeight
+		})
+		totalScroll -= win.h
+		return totalScroll
+	}
+}
+
+Scroll.init()
+
+//add GSAP logo
+logo("dark")
